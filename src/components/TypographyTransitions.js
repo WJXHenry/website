48e7458864c17468bd0b9cf167147223b9912af3
WJXHenry <wjxhenry@gmail.com>
2019-05-24 22:30:48 -0600

    Typing animation

--- src/components/TypographyTransitions.js
+++ src/components/TypographyTransitions.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import { Fade, Typography } from '@material-ui/core';
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 
 // Define the styling for all the Typography components with transitions here
 const typographyStyles = makeStyles({
@@ -23,5 +25,32 @@ export function TypographyFade(props) {
     <Fade in style={{ transitionDelay: delay }} timeout={timeout}>
       <Typography className={classes.root} {...other} />
     </Fade>
+  );
+}
+
+TypographyTyped.propTypes = {
+  delay: PropTypes.number
+};
+
+const cursor = {
+  show: true,
+  blink: true,
+  element: '|',
+  hideWhenDone: true,
+  hideWhenDoneDelay: 8000
+};
+
+export function TypographyTyped(props) {
+  const { delay, ...other } = props;
+  const classes = typographyStyles();
+  return (
+    <Typography className={classes.root} {...other}>
+      <Typist
+        avgTypingDelay={35}
+        cursor={cursor}
+        startDelay={delay}
+        {...other}
+      />
+    </Typography>
   );
 }
